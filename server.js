632eb0a132b0cf8aa169649f7a9e04a1595edb62
009kings <009kings@gmail.com>
2019-11-05 12:00:16 -0800

    finished server and route stubs

---
+++ server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const expressJWT = require('express-jwt');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', () => console.log(`Connected to MongDB on ${db.host}:${db.port}...`));
+db.on('error', err => console.log(`💩DB error:\n${err}`));
+
+app.use('/auth', require('./routes/auth'));
+
+app.listen(process.env.PORT, ()=>console.log(`🎧☕️You're listening to the smooth sounds of port ${process.env.PORT} ☕️🎧`))
